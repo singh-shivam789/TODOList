c386e1fbbf375c9ff3242de18d304d25b7b60b1c
Shivam Singh <shivamsinghgbpit@gmail.com>
2020-09-26 16:32:26 +0530

    add title and check styles

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -33,14 +33,17 @@ for (i = 0; i < category_boxes.length; i++) {
 
 //handling hover on title
 var title_box = document.getElementById('title-box');
-var title_text = document.getElementById('title-text');
+var title_text1 = document.getElementById('title-text1');
+var title_text2 = document.getElementById('title-text2');
 
 title_box.addEventListener('mouseover', function() {
-    title_text.style.color = 'yellow';
+    title_text1.style.color = 'yellow';
+    title_text2.style.color = 'yellow';
     title_box.style.boxShadow = '0px 0px 3px 3px #6a2c70'
 });
 title_box.addEventListener('mouseout', function() {
-    title_text.style.color = '#3f3f44';
+    title_text1.style.color = '#3f3f44';
+    title_text2.style.color = '#3f3f44';
     title_box.style.boxShadow = 'none'
 });
 
